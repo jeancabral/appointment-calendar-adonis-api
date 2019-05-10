47aabbfa058b042caaa66ec74fc9fdbfcb09e9d9
Jean Cabral <jeancabral@gmail.com>
2019-05-10 11:44:11 -0300

    Adicionando JWT Token e Recuperação de Senha

--- start/routes.js
+++ start/routes.js
@@ -3,3 +3,6 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store')
+
+Route.post('passwords', 'ForgotPasswordController.store')
